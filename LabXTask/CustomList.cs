06aa762ff85dc39e118681f3ab1707a47ab71c0e
Nyakoren <minenkov_vladimir2003@inbox.ru>
2024-10-23 16:33:45 +0300

    Task 2 commit.
    Changed list to generic. Added methods IndexOf, Find and Sort

--- LabXTask/CustomList.cs
+++ LabXTask/CustomList.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-public class CustomList
+public class CustomList<T>
 {
-    private int[] _list = new int[4];
+    private T[] _list = new T[4];
     private int _length = 0;
     private int _maxSize = 4;
 
@@ -15,16 +16,16 @@ public class CustomList
         get => _list.Length;
     }
 
-    public int? Get(int index)
+    public T Get(int index)
     {
         if ((index >= 0) && (index <= this._length))
         {
             return _list[index - 1];
         }
-        return null;
+        return default;
     }
 
-    public void Add(int num)
+    public void Add(T num)
     {
         if (_length == _maxSize)
         {
@@ -38,11 +39,11 @@ public class CustomList
             _list[_length - 1] = num;
         }
     }
-    public void RemoveValue(int val)
+    public void RemoveValue(T val)
     {
         for (int i = 0; i < _length; i++)
         {
-            if (_list[i] == val)
+            if (_list[i].Equals(val))
             {
                 ShiftLeft(i);
             }
@@ -56,7 +57,7 @@ public class CustomList
             ShiftLeft(index - 1);
         }
     }
-    public void Insert(int index, int val)
+    public void Insert(int index, T val)
     {
         ShiftRight(index);
         _list[index - 1] = val;
@@ -71,7 +72,7 @@ public class CustomList
             }
             if (i == _length)
             {
-                _list[i] = 0;
+                _list[i] = default;
                 break;
             }
             _list[i] = _list[i + 1];
@@ -94,14 +95,14 @@ public class CustomList
         {
             _list[i] = _list[i - 1];
         }
-        _list[startIndex - 1] = 0;
+        _list[startIndex - 1] = default;
     }
 
 
     private void ExtendList()
     {
         _maxSize *= 2;
-        int[] extended = new int[_maxSize];
+        T[] extended = new T[_maxSize];
         for (int i = 0; i < _length; i++)
         {
             extended[i] = _list[i];
@@ -111,7 +112,7 @@ public class CustomList
     private void CompressList()
     {
         _maxSize /= 2;
-        int[] compressed = new int[_maxSize];
+        T[] compressed = new T[_maxSize];
         for (int i = 0; i < _length; i++)
         {
             compressed[i] = _list[i];
@@ -123,7 +124,7 @@ public class CustomList
     {
         _maxSize = 4;
         _length = 0;
-        _list = new int[4];
+        _list = new T[4];
     }
 
     public override string ToString()
@@ -139,6 +140,46 @@ public class CustomList
         }
         resString += _list[_length - 1].ToString();
         return resString;
+    }
+
+    public int IndexOf(T item)
+    {
+        for(int i=0; i < _length; i++)
+        {
+            if (_list[i].Equals(item))
+            {
+                return i+1;
+            }
+        }
+        return -1;
+    }   
+    
+    public T? Find(System.Func<T, bool> findFunc)
+    {
+       for(int i=0;i < _length; i++)
+        {
+            if (findFunc.Invoke(_list[i]) == true)
+            {
+                return _list[i];
+            }
+        }
+        return default;
+    }
+
+    public void Sort(System.Func<T, T, int> equalFunc)
+    {
+        for(int i=0;i< _length; i++)
+        {
+            for(int j=i; j< _length-i; j++)
+            {
+                if (equalFunc(_list[j], _list[j + 1]) == 1)
+                {
+                    T temp = _list[j];
+                    _list[j] = _list[j + 1];
+                    _list[j + 1] = temp;
+                }
+            }
+        }
     }
 }
 
